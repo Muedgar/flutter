98edd0d5cf58bcc0f9329cf623bf35ad366b3dc1
MuEdgar <edgarmutangana@gmail.com>
2026-06-17 09:04:05 +0200

    added more exercises

--- lib/main.dart
+++ lib/main.dart
@@ -2,66 +2,61 @@
 
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MaterialApp(home: TaskCardExercise()));
+class Todo {
+  String id;
+  String name;
+  String completed;
+
+
+  Todo({required this.id, required this.name, required this.completed});
 }
 
-class TaskCardExercise extends StatelessWidget {
-  const TaskCardExercise({super.key});
+class TodoPage extends StatefulWidget {
+  const TodoPage({super.key})
+
+  State<TodoPage> _createState: State<_TodoPageState()>;
+}
+
+class _TodoPageState extends State<TodoPage> {
+
+  TextEditingController controller = TextEditingController();
+
+  List<Todo> todos = [];
+
+  addTodo() {
+
+  }
 
   @override
   Widget build(BuildContext context) {
-    const completed = true;
-
-    return Scaffold(
-      backgroundColor: const Color(0xfff4f6fb),
-      body: Center(
-        child: Container(
-          margin: const EdgeInsets.all(20),
-          padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 10),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(20),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.05),
-                blurRadius: 12,
-                offset: const Offset(0, 6)
-              )
-            ]
-          ),
-          child: Row(
-            children: [
-              Checkbox(
-                value: completed,
-                onChanged: (_) {},
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(6)
-                )
+    return Center(
+      child: Padding(
+        padding: EdgeInsetsGeometry.all(1.4),
+        child: Column(
+          children: [
+            Row(
+          children: [
+            TextField(
+              controller: controller,
+              style: TextStyle(
+                color: Color.fromARGB(1, 248, 123, 231)
               ),
-              const Expanded(
-                child: Text(
-                  'Study Flutter UI',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
-                    decoration: TextDecoration.lineThrough,
-                    color: Colors.grey
-                  )
-                )
               ),
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.edit_rounded, color: Colors.indigo,)
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.delete_rounded, color: Colors.redAccent) 
-              )
-            ],
-          )
-        )
-      )
-    );
+            ElevatedButton(
+              onPressed: addTodo(), 
+              child: Text('Add todo'),
+              style: ButtonStyle(
+                backgroundColor: Color.fromARGB(5, 128, 181, 64),
+                foregroundColor: Color.fromARGB(255, 0, 0, 0)
+              ),)
+          ],
+        ),
+        Column(children: [
+          ListView.builder(itemBuilder: () {
+            ite
+          })
+        ],)
+          ],)
+    ));
   }
 }
