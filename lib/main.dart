dccfb578b897ccf1be6d6c2fe4d1c1db896faa8c
MuEdgar <edgarmutangana@gmail.com>
2026-07-20 15:10:04 +0200

    more exercises

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,52 @@
-
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: HeaderExerciseTwo()));
+  runApp(const MaterialApp(home: BreathingButton()));
 }
 
-class HeaderExerciseTwo extends StatelessWidget {
-  const HeaderExerciseTwo({super.key});
+class BreathingButton extends StatefulWidget {
+  const BreathingButton({super.key});
 
-  @override 
+  @override
+  State<BreathingButton> createState() => _BreathingButtonState();
+}
+
+class _BreathingButtonState extends State<BreathingButton>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> scale;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    )..repeat(reverse: true);
+
+    scale = Tween<double>(begin: 0.9, end: 1.08).animate(controller);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 191, 199, 219),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Row(
-            children: [
-              Container(
-                padding: const EdgeInsets.all(14),
-                decoration: BoxDecoration(
-                  color: Colors.indigo,
-                  borderRadius: BorderRadius.circular(18)
-                ),
-                child: const Icon(
-                  Icons.checklist_rounded,
-                  color: Colors.white,
-                  size: 30
-                )
-              ),
-              const SizedBox(width: 14),
-              const Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'My Tasks',
-                    style: TextStyle(
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
-                    )
-                  ),
-                  Text(
-                    'Stay organized today',
-                    style: TextStyle(color: Colors.grey)
-                  )
-                ]
-              )
-            ]
+      backgroundColor: Colors.indigo,
+      body: Center(
+        child: ScaleTransition(
+          scale: scale,
+          child: ElevatedButton(
+            onPressed: () {},
+            child: const Text("Start"),
           ),
-        )
-      )
+        ),
+      ),
     );
   }
 }
