d86296dfb61e913142ca364f3565f78d5aae6c7c
MuEdgar <edgarmutangana@gmail.com>
2026-09-03 23:42:20 +0200

    Learning flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,100 @@
-
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: TodoPage(),));
+  runApp(MaterialApp(home: TheBox()));
 }
 
-class Todo {
-    String id;
-    String title;
-    bool completed;
+class TheBox extends StatefulWidget {
+  const TheBox({super.key});
 
-    Todo({required this.id, required this.title, required this.completed});
+  @override
+  State<TheBox> createState() => _TheBoxState();
 }
 
-class TodoPage extends StatefulWidget {
-    const TodoPage({super.key});
-
-    @override
-    State<TodoPage> createState() => _TodoPageState();
-}
-
-class _TodoPageState extends State<TodoPage> {
-    List<Todo> todos = [
-        Todo(id: DateTime.now().toString(), title: 'Cook', completed: false)
-    ];
-
-    
-
-    
-
-    @override
-    Widget build(BuildContext context) {
-        TextEditingController controller = TextEditingController();
-void addTodo() {
-        setState(() {
-            todos.add(Todo(id: DateTime.now().toString(), title: controller.text, completed: false));
-        });
-    }
-        return Scaffold(
-            appBar: AppBar(
-                title: Text('Todo'),
+class _TheBoxState extends State<TheBox> {
+  bool moveRight = false;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("The box"),),
+      body: Column(
+        children: [
+          Expanded(
+            child: AnimatedAlign(
+              alignment: moveRight ? Alignment.topLeft : Alignment.bottomRight, 
+              duration: const Duration(milliseconds: 500),
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 500),
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: moveRight ? Colors.orange : Colors.blue,
+                  borderRadius: BorderRadius.circular(moveRight ? 0 : 50)
+                )
+              )
             ),
-            body: Padding(
-                padding: EdgeInsets.all(4),
-                child: Column(
-                    children: [
-                        Row(
-                            children: [
-                                Expanded(
-                                child: TextField(controller: controller)
-                            ),
-                            ElevatedButton(child: Text('Add todo'), onPressed: () => addTodo(),)
-                            ],
-                        ),
-                        Expanded(
-                            child: ListView.builder(
-                                itemCount: todos.length,
-                                itemBuilder:(_, index) {
-                                  Todo todo = todos[index];
-                                  return ListTile(
-                                    title: Text(todo.title),
-                                  );
-                                },)
-                        )
-                    ],
-                ),
-            ),
-        );
-    }
+          ),
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                moveRight = !moveRight;
+              });
+            },
+            child: const Text("Move")), 
+        ]
+      ),
+    );
+  }
 }
+
+
+
+
+
+// box animation left to right, top to bottom
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MaterialApp(home: TheBox()));
+// }
+
+// class TheBox extends StatefulWidget {
+//   const TheBox({super.key});
+
+//   @override
+//   State<TheBox> createState() => _TheBoxState();
+// }
+
+// class _TheBoxState extends State<TheBox> {
+//   bool moveRight = false;
+  
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text("The box"),),
+//       body: Column(
+//         children: [
+//           Expanded(
+//             child: AnimatedAlign(
+//               alignment: moveRight ? Alignment.topLeft : Alignment.bottomRight, 
+//               duration: const Duration(milliseconds: 500),
+//               child: Container(
+//                 width: 100,
+//                 height: 100,
+//                 color: Colors.blue
+//               )
+//             ),
+//           ),
+//           ElevatedButton(
+//             onPressed: () {
+//               setState(() {
+//                 moveRight = !moveRight;
+//               });
+//             },
+//             child: const Text("Move")), 
+//         ]
+//       ),
+//     );
+//   }
+// }
